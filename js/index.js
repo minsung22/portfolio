623ea2dca8dc3a25a12942e7e16e9f83f7413625
minsung22 <minsungg222@gmail.com>
2020-11-27 14:50:45 +0900

    ddd

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ window.addEventListener('DOMContentLoaded',function(){
                 clearTimeout(aa);
                 aa = setTimeout(funWheel,100,e);
             })
+            
 
             function funWheel(e){
                 if(e.wheelDelta < 0){
@@ -46,58 +47,118 @@ window.addEventListener('DOMContentLoaded',function(){
                 indiBox.style.transform = "translateY(-"+indiMove+"px)";
 
             }
+
+            
         });
+        prev.addEventListener('click',funPrev);
+        next.addEventListener('click',funNext);
 
+            function funNext(e){
+                e.preventDefault();
+                console.log(slideW);
+                move += slideW;
+                upMove += whiteT;
+                indiMove += indiT;
+                slideInner.style.transform = "translateX(-"+move+"px)";
+                whiteW.style.transform = "translateY(-"+upMove+"px)";
+                blackW.style.transform = "translateY(-"+upMove+"px)";
+                indiBox.style.transform = "translateY(-"+indiMove+"px)";
+            }
     
+            function funPrev(e){
+                e.preventDefault();
+                console.log(slideW);
+                move -= slideW;
+                upMove -= whiteT;
+                indiMove -= indiT;
+                slideInner.style.transform = "translateX(-"+move+"px)";
+                whiteW.style.transform = "translateY(-"+upMove+"px)";
+                blackW.style.transform = "translateY(-"+upMove+"px)";
+                indiBox.style.transform = "translateY(-"+indiMove+"px)";
+            }
 
-//works페이지 팝업
+        
+
+//페이지 팝업
     var worksPop = document.querySelector('.works');
-    var popup2 = document.querySelector('.works .w-title .title h2')
+    var popup1 = document.querySelector('.works .w-title .title h2')
+    var aboutPop = document.querySelector('.About-me');
+    var popup2 = document.querySelector('.About-me .a-title h2')
+    var contactPop = document.querySelector('.contact-me');
+    var popup3 = document.querySelector('.contact-me .c-title .title h2')
 
     for(var i=0;i<whiteWEl.length;i++){
-        if(i == 0){
-            whiteWEl[i].addEventListener('click',funTab);
-        }
+            whiteWEl[0].addEventListener('click',funTab);
+            whiteWEl[1].addEventListener('click',funTab2);
+            whiteWEl[2].addEventListener('click',funTab3);
     }
-    
+    //works
     function funTab(e){
-
         e.preventDefault();
-
         $('.works').show();
 
         setTimeout(function(){ 
-
             worksPop.classList.add('active')
-            popup2.classList.add('active');
-
+            popup1.classList.add('active');
         },10);
-        
     }
+    //about me
+    function funTab2(e){
+        e.preventDefault();
+        $('.About-me').show();
 
+        setTimeout(function(){ 
+            aboutPop.classList.add('active')
+            popup2.classList.add('active');
+        },10);
+    }
+    //contact me
+    function funTab3(e){
+        e.preventDefault();
+        $('.contact-me').show();
 
-
+        setTimeout(function(){ 
+            contactPop.classList.add('active')
+            popup3.classList.add('active');
+        },10);
+    }
 
 // X누르면 팝업내려가게
-    var burgerX = document.querySelectorAll('.burger');
+    var burgerX1 = document.querySelector('.works .burger');
+    var burgerX2 = document.querySelector('.About-me .burger');
+    var burgerX3 = document.querySelector('.contact-me .burger');
     var workEl = document.querySelectorAll('.work-list .work-item');
-
-    for(var i=0;i<burgerX.length;i++){
-        burgerX[i].addEventListener('click',funburgerX)
-    }
-
-    function funburgerX(e){
+    
+        burgerX1.addEventListener('click',funburgerX1)
+        burgerX2.addEventListener('click',funburgerX2)
+        burgerX3.addEventListener('click',funburgerX3)
+    
+    function funburgerX1(e){
         e.preventDefault();
     
         worksPop.classList.remove('active');
-        popup2.classList.remove('active');
+        popup1.classList.remove('active');
         
         $('.works').hide();
     }
-
+    function funburgerX2(e){
+        e.preventDefault();
+    
+        aboutPop.classList.remove('active');
+        popup2.classList.remove('active');
+        
+        $('.About-me').hide();
+    }
+    function funburgerX3(e){
+        e.preventDefault();
+    
+        contactPop.classList.remove('active');
+        popup3.classList.remove('active');
+        
+        $('.contact-me').hide();
+    }
 
 //About me 컨텐츠들 나타나게
-
 var jsIcon = document.querySelector('.greeting .icon');
 var jsTop = jsIcon.offsetTop;
 var nameTit = document.querySelector('.greeting .name_tit');
@@ -114,39 +175,40 @@ var home = document.querySelector('.home h2');
 var homeTop = home.offsetTop;
 
 var winH = window.innerHeight/5;
-window.addEventListener('scroll',aboutScroll);
+aboutPop.addEventListener('scroll',aboutScroll);
 
-function aboutScroll(){
+function aboutScroll(e){
+    var scrollY = $('.About-me').scrollTop();
     
-        if(window.scrollY >= jsTop + winH){
+        if(scrollY >= jsTop + winH){
             jsIcon.classList.add('active'); 
         }
-        if(window.scrollY >= nameTop + winH + 200){
+        if(scrollY >= nameTop + winH + 200){
             nameTit.classList.add('active');
         }
-        if(window.scrollY >= subTop + winH + 100){
+        if(scrollY >= subTop + winH + 100){
             subTit.classList.add('active');
         }
         setTimeout(function(){
-            if(window.scrollY >= subTop + winH + 200){
+            if(scrollY >= subTop + winH + 200){
                 subList.classList.add('active');
             }
         },800);
         setTimeout(function(){
-            if(window.scrollY >= infoTop + winH){
+            if(scrollY >= infoTop + winH){
                 myInfo.classList.add('active');
             }
             setTimeout(function(){
-                if(window.scrollY >= graphTop + winH){
+                if(scrollY >= graphTop + winH){
                     graph.classList.add('active');
                 }
             },800);
             setTimeout(function(){
-                if(window.scrollY >= graphTop + winH + 200){
+                if(scrollY >= graphTop + winH + 200){
                     skillTxt.classList.add('active');
                 }
                 setTimeout(function(){
-                    if(window.scrollY >= homeTop + 100){
+                    if(scrollY >= homeTop + 100){
                         home.classList.add('active');
                     }
                 },1000);
@@ -155,7 +217,6 @@ function aboutScroll(){
 }
 
 // Contact me 컨텐츠들 나오게
-
 var cList = document.querySelectorAll('.c-list > div');
 var cListArr = [];
 var cName = document.querySelector('.cName');
@@ -173,23 +234,25 @@ setTimeout(function(){
     });
 },500);
 
-
 var winH = window.innerHeight/5;
-    window.addEventListener('scroll',function(){
-        
-        if(window.scrollY >= cListArr[0] + winH){
+    contactPop.addEventListener('scroll',function(){
+    
+        var scrollY = $('.contact-me').scrollTop();
+        console.log(scrollY);
+
+        if(scrollY >= cListArr[0] + winH){
             cName.classList.add('active');
             cLocation.classList.add('active');
         }
-        if(window.scrollY >= cListArr[1] + winH + 100){
+        if(scrollY >= cListArr[1] + winH + 100){
             cBirth.classList.add('active');
             cEmail.classList.add('active');
         }
-        if(window.scrollY >= cListArr[2] + winH + 100){
+        if(scrollY >= cListArr[2] + winH + 100){
             cAdress.classList.add('active');
             cPhone.classList.add('active');
         }
-        if(window.scrollY >= snsTop + winH){
+        if(scrollY >= snsTop + winH){
             snsAdrs.classList.add('active');
         }
 });
