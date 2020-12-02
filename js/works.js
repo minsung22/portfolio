513cad496e076eea3ba0310adf89351d647d55b5
minsung22 <minsungg222@gmail.com>
2020-12-02 17:15:46 +0900

    df

--- js/works.js
+++ js/works.js
@@ -2,27 +2,78 @@ window.addEventListener('DOMContentLoaded',function(){
 
     var nameTit = document.querySelector('.view_tit_area .name_tit');
     var launch = document.querySelector('.container .view_tit_area a');
-    var info = document.querySelector('.view_info .info');
+    var info = document.querySelector('.view_info');
     var skillChart = document.querySelector('.skill_chart');
     var font01 = document.querySelector('.font .font-01');
     var font02 = document.querySelector('.font .font-02');
     var colorInfo = document.querySelectorAll('.color .aos-init');
-    var mac = document.querySelector('.view_main_mac p');
+    var mac = document.querySelector('.view_main_mac');
     var winH = window.innerHeight;
 
     window.addEventListener('scroll',funScroll);
 
     function funScroll(){
-        console.log(scrollY);
+        console.log(nameTit.offsetTop - winH);
         if(scrollY > nameTit.offsetTop - winH){
             nameTit.classList.add("active")
+        }if(scrollY == 0){
+            nameTit.classList.remove("active");
         }
         if(scrollY > launch.offsetTop - winH){
             launch.classList.add("active")
+        }else{
+            launch.classList.remove("active")
         }
-        if(scrollY > info.offsetTop - winH){
+        if(scrollY > info.offsetTop - winH + 100){
             info.classList.add("active")
+        }else{
+            info.classList.remove("active")
         }
+        if(scrollY > skillChart.offsetTop - winH + 100){
+            skillChart.classList.add("active")
+        }else{
+            skillChart.classList.remove("active")
+        }
+        if(scrollY > font01.offsetTop - winH){
+            font01.classList.add("active")
+        }else{
+            font01.classList.remove("active")
+        }
+        if(scrollY > font02.offsetTop - winH){
+            font02.classList.add("active")
+        }else{
+            font02.classList.remove("active")
+        }
+        for(var i=0;i<colorInfo.length;i++){
+            if(scrollY > colorInfo[i].offsetTop - winH + 100){
+                colorInfo[i].classList.add("active")
+            }else{
+                colorInfo[i].classList.remove("active")
+            }
+        }
+        if(scrollY > mac.offsetTop - winH + 400){
+            mac.classList.add("active")
+        }else{
+            mac.classList.remove("active")
+        }
+        if(scrollY > 200){
+            topBtn.classList.add("active")
+        }else{
+            topBtn.classList.remove("active");
+        }
+    }
+
+    var topBtn = document.querySelector('.fixed_btn');
+
+    topBtn.addEventListener('click',funClick);
+
+    function funClick(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+			scrollTop : 0
+		}, 400);
+		return false;
     }
 
 
